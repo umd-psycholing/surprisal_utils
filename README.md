4146debfebb3c479f6795eaad81b322dc8c2a682
Sathvik Nair <sathviknair@berkeley.edu>
2024-11-06 16:09:59 -0500

    Use wordfreq to compute frequencies

--- README.md
+++ README.md
@@ -4,14 +4,16 @@ To generate a CSV of surprisals and RTs, run `merge_with_rt_data` in `surprisal_
 
 Here's an example of code that will generate a dataset to be run through the `surprisal_analysis` notebook. Importantly, the RT data are marked with exclusion criteria, and the predictors are at the word level. Please let me know if you need code to address dealing with subword tokens and I'll add it here as well. One of the control predictors is log frequency. I generated it by taking unigram probabilities from an n-gram model trained on the publicly available version of COCA (9.5 million words). Please let me know if you need the file, or want to get it working with other datasets.
 
+If you don't have them, run `pip install pandas`, `pip install numpy`, and `pip install wordfreq` in your terminal.
+
 ```
 import surprisal_utils
 import pandas as pd
 
 predictors = pd.DataFrame("lexical_predictors.csv")
 rts = pd.DataFrame("rt_corpus.csv")
-frequency_file = "/fs/clip-psych/sathvik/reading_time_data/word_freqs.txt"
 
-combined_data = surprisal_utils.merge_with_rt_data(predictors, rts, "surprisal", frequency_file)
+combined_data = surprisal_utils.merge_with_rt_data(predictors, rts, "surprisal", spillover_count = 1,
+ language = "en", frequency_file = None)
 combined_data.to_csv("surprisal_rt_corpus.csv", index = False)
 ```
--- surprisal_utils.py
+++ surprisal_utils.py
@@ -1,5 +1,9 @@
-import pandas as pd
 import string
+from typing import Optional
+
+import numpy as np
+import pandas as pd
+from wordfreq import word_frequency
 
 def clean_str(token):
     return token.replace('[^\w\s]','').replace(",", "").replace(".", "").lower()
@@ -8,9 +12,16 @@ def clean_str(token):
 def word_length(rt_data: pd.DataFrame, token_col: str):
     return rt_data.apply(lambda row: len(row[token_col]), axis = 1)
 
-def join_log_freq(filepath: str, rt_data: pd.DataFrame):
-    freq_table = pd.read_table(filepath, delim_whitespace=True, names=('prob', 'word', 'backoff_weight'))
-    rt_data = rt_data.merge(freq_table[['prob', 'word']], how = 'left', left_on = 'token', right_on = 'word')
+def compute_log_frequency(word, language):
+    freq = word_frequency(word, language)
+    return np.log2(freq)
+
+def join_log_freq(rt_data: pd.DataFrame, from_file : bool, lang : Optional[str], filepath : Optional[str]):
+    if from_file:
+        freq_table = pd.read_table(filepath, delim_whitespace=True, names=('prob', 'word', 'backoff_weight'))
+        rt_data = rt_data.merge(freq_table[['prob', 'word']], how = 'left', left_on = 'token', right_on = 'word')
+    else:
+        rt_data['token'].apply(compute_log_frequency, language = lang)
     rt_data.rename(columns={'prob': 'log_freq'}, inplace = True)
     return rt_data
 
@@ -26,11 +37,15 @@ IMPORTANT: this function assumes that each row of the surprisal df has predictor
  associated with the same word as the RT data, and the RT data has values to exclude (marked under an `exclude` column).
  See the next function to generate the exclude value for an RT dataset (following Goodkind & Bicknell)
 """
-def merge_with_rt_data(surprisal_df : pd.DataFrame, rt_data : pd.DataFrame, predictor_name: str, frequency_file : str):
+def merge_with_rt_data(surprisal_df : pd.DataFrame, rt_data : pd.DataFrame, predictor_name: str, spillover_count : int,
+                       frequency_file : Optional[str], language : Optional[str]):
     rt_data['surprisal'] = surprisal_df[predictor_name]
     rt_data['word_length'] = word_length(rt_data, 'token')
-    rt_data = join_log_freq(frequency_file, rt_data)
-    rt_data = prev_token_predictors(rt_data, 3)
+    if frequency_file:
+        rt_data = join_log_freq(rt_data, True, filepath=frequency_file)
+    else:
+        rt_data = join_log_freq(rt_data, False, language = language)
+    rt_data = prev_token_predictors(rt_data, spillover_count)
     rt_data = rt_data[rt_data['exclude'] == 0].dropna()
     return rt_data
 
